7c00ebc7014b1f03889af0da4314fb446a3f95f5
Phoenix-Resto <phoenixresto12@gmail.com>
2024-10-24 13:12:46 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ function openPdf()
 {
 var omyFrame = document.getElementById("myFrame");
 omyFrame.style.display="block";
-omyFrame.src = "https://docs.google.com/viewer?url=https://raw.githubusercontent.com/phoenix-resto/Phoenix-Resto/images/phoenix-menu.pdf";
+omyFrame.src = "https://docs.google.com/viewer?url=https://raw.githubusercontent.com/phoenix-resto/Phoenix-Resto/images/phoenix-menu.pdf&embedded=true";
 omyFrame.scrollIntoView({ behavior: 'smooth' });
 }
 
@@ -10,6 +10,6 @@ function openPdf1()
 {
 var omyFrame = document.getElementById("myFrame");
 omyFrame.style.display="block";
-omyFrame.src = "https://docs.google.com/viewer?url=https://raw.githubusercontent.com/phoenix-resto/Phoenix-Resto/images/combo-menu.pdf";
+omyFrame.src = "https://docs.google.com/viewer?url=https://raw.githubusercontent.com/phoenix-resto/Phoenix-Resto/images/combo-menu.pdf&embedded=true";
 omyFrame.scrollIntoView({ behavior: 'smooth' });
 }
