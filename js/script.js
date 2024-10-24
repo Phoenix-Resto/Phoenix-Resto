7de7a227e9308dda6789c7d67c0f9d4679d6c60e
Phoenix-Resto <phoenixresto12@gmail.com>
2024-10-24 12:17:52 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+function openPdf()
+{
+var omyFrame = document.getElementById("myFrame");
+omyFrame.style.display="block";
+omyFrame.src = "./images/phoenix-menu.pdf";
+omyFrame.scrollIntoView({ behavior: 'smooth' });
+}
+
+function openPdf1()
+{
+var omyFrame = document.getElementById("myFrame");
+omyFrame.style.display="block";
+omyFrame.src = "./images/combo-menu.pdf";
+omyFrame.scrollIntoView({ behavior: 'smooth' });
+}
